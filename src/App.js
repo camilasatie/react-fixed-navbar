a346a7fa60bbaaacc5867c2c3db4e3e60f1ffd6d
Camila Satie <camilasatie.dev@gmail.com>
2021-01-17 10:46:30 -0300

    add fixed-navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackgroundColor = () => {
+    if (window.scrollY >= 300) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  window.addEventListener("scroll", changeBackgroundColor);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <>
+      <header>
+        <h1>Welcome</h1>
+      </header>
+      <nav className={navbar ? "navbar active" : "navbar"}>
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+      <main className="main">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+      </main>
+    </>
   );
 }
